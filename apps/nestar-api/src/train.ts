ac01d862a07ba5309730ff849ff447c6b8857244
amurkhon <akramjonovamurxon02@gmail.com>
2025-06-08 17:16:20 +0900

    feat: create Schema Models and project enums

--- apps/nestar-api/src/train.ts
+++ apps/nestar-api/src/train.ts
@@ -1,12 +1,21 @@
 // TASK-ZM
 
-function reverseNumbers(number: number) {
-    const str = number.toString();
-    const list  = str.split('').reverse();
-    return list.reduce((total, value) => {
-        return total + value;
-    })
+// function reverseNumbers(number: number) {
+//     const str = number.toString();
+//     const list  = str.split('').reverse();
+//     return list.reduce((total, value) => {
+//         return total + value;
+//     })
+// }
+
+// const result = reverseNumbers(12345);
+// console.log("result: ", result);
+
+// TASK-ZN
+
+function rotateArray(array: number[], index: number) {
+    return array.slice(index).concat(array.slice(0, index));
 }
 
-const result = reverseNumbers(12345);
+const result = rotateArray([1,2,3,4,5,6], 4);
 console.log("result: ", result);
