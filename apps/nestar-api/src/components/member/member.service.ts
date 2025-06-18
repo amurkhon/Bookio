a1f386be0e628c821cd97cff24c706a42507c5bd
amurkhon <akramjonovamurxon02@gmail.com>
2025-06-18 17:37:26 +0900

    feat: develop getAgents GraphQl api

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -2,10 +2,10 @@ import { BadRequestException, Injectable, InternalServerErrorException } from '@
 import { InjectModel } from '@nestjs/mongoose';
 import MemberSchema from '../../schemas/Member.model';
 import { Model, ObjectId } from 'mongoose';
-import { Member } from '../../libs/dto/member/member';
-import { MemberInput, LoginInput } from '../../libs/dto/member/member.input';
-import { MemberStatus } from '../../libs/enums/member.enum';
-import { Message } from '../../libs/enums/common.enum';
+import { Member, Members } from '../../libs/dto/member/member';
+import { MemberInput, LoginInput, AgentsInquery } from '../../libs/dto/member/member.input';
+import { MemberStatus, MemberType } from '../../libs/enums/member.enum';
+import { Direction, Message } from '../../libs/enums/common.enum';
 import { AuthService } from '../auth/auth.service';
 import { MemberUpdate } from '../../libs/dto/member/member.update';
 import { T } from '../../libs/types/common';
@@ -106,8 +106,35 @@ export class MemberService {
                 .exec();
                 targetMember.memberViews++;
             }
+            // meLiked
+            // meFollwed
         }
         return targetMember;
+    }
+
+    public async getAgents(memberId: ObjectId, input: AgentsInquery ): Promise<Members> {
+        const { text } = input.search;
+        const match: T = { memberType: MemberType.AGENT, memberStatus: MemberStatus.ACTIVE};
+        const sort: T = { [input?.sort ?? 'createdAt']: input?.direction ?? Direction.DESC};
+
+        if(text) match.memberNick = { $regex: new RegExp(text, 'i') };
+        console.log('match: ', match);
+
+        const result = await this.memberModel
+            .aggregate([
+                { $match: match },
+                { $sort: sort },
+                {
+                    $facet: {
+                        list: [{ $skip: (input.page - 1) * input.limit}, { $limit: input.limit }],
+                        metaCounter: [{ $count: 'total' }],
+                    }
+                }
+            ])
+            .exec()
+        if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+
+        return result[0];
     }
 
     public async updateMemberByAdmin(): Promise<string> {
