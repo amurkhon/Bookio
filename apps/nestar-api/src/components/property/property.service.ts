26ff20008428f0d87ba0ad94c5f903fce6d7d131
amurkhon <akramjonovamurxon02@gmail.com>
2025-06-23 14:38:38 +0900

    feat: removePropertyByAdmin GraphQL api

--- apps/nestar-api/src/components/property/property.service.ts
+++ apps/nestar-api/src/components/property/property.service.ts
@@ -270,4 +270,12 @@ export class PropertyService {
 
         return updatedProperty;
     }
+
+    public async removePropertyByAdmin(propertyId: ObjectId): Promise<Property> {
+        const search: T = { _id: propertyId, propertyStatus: PropertyStatus.DELETE };
+        const result = await this.propertyModel.findOneAndDelete(search).exec();
+        if(!result) throw new InternalServerErrorException(Message.REMOVE_FAILED);
+
+        return result;
+    }
 }
